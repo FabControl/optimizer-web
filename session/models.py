f1bfc1cf4d1c9ed2fba62650bc63b0504f9ee516
egilsar <egils.arajs@massportal.com>
2019-08-19 17:29:13 +0300

    Added a test_info model and migration

--- session/models.py
+++ session/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from datetime import datetime
-from .choices import TEST_NUMBER_CHOICES, TARGET_CHOICES, SLICER_CHOICES, TOOL_CHOICES, FORM_CHOICES
+from .choices import TEST_NUMBER_CHOICES, TARGET_CHOICES, SLICER_CHOICES, TOOL_CHOICES, FORM_CHOICES, UNITS
+from django.conf import settings
+from optimizer_api import ApiClient
 
 # Create your models here.
+
+
 class Material(models.Model):
     size_od = models.DecimalField(default=1.75, max_digits=3, decimal_places=2)
     name = models.CharField(max_length=60)
@@ -11,9 +15,24 @@ class Material(models.Model):
     def __str__(self):
         return "{} ({} mm)".format(self.name, str(self.size_od))
 
+    @property
+    def __json__(self):
+        output = {
+                     "size_od": self.size_od,
+                     "name": self.name
+                 }
+        return output
+
 
 class Nozzle(models.Model):
     size_id = models.DecimalField(default=0.4, decimal_places=1, max_digits=2)
+
+    @property
+    def __json__(self):
+        output = {
+            "size_id": self.size_id
+        }
+        return output
 
 
 class Extruder(models.Model):
@@ -24,14 +43,16 @@ class Extruder(models.Model):
     temperature_max = models.IntegerField(default=350)
     part_cooling = models.BooleanField(default=True)
     nozzle = models.ForeignKey(Nozzle, on_delete=models.CASCADE)
-    
-    #       "extruder": {
-    #         "tool": "",
-    #         "temperature_max": 350,
-    #         "part_cooling": true,
-    #         "nozzle": {
-    #           "size_id": 0.8
-    #         }
+
+    @property
+    def __json__(self):
+        output = {
+            "tool": self.tool,
+            "temperature_max": self.temperature_max,
+            "part_cooling": self.part_cooling,
+            "nozzle": self.nozzle.__json__
+        }
+        return output
 
 
 class Chamber(models.Model):
@@ -39,19 +60,27 @@ class Chamber(models.Model):
     tool = models.CharField(max_length=3, choices=TOOL_CHOICES, blank=True)
     gcode_command = models.CharField(max_length=40, default="M141 S$temp")
     temperature_max = models.IntegerField(default=80)
-    #       "chamber": {
-    #         "tool": "",
-    #         "gcode_command": "M141 S$temp",
-    #         "temperature_max": 80,
-    #         "chamber_heatable": false
-    #       },
+
+    @property
+    def __json__(self):
+        output = {
+            "tool": self.tool,
+            "gcode_command": self.gcode_command,
+            "temperature_max": self.temperature_max,
+            "chamber_heatable": self.chamber_heatable
+          }
+        return output
 
 
 class Printbed(models.Model):
     printbed_heatable = models.BooleanField(default=True)
-    # "printbed": {
-    #         "printbed_heatable": true
-    #       }
+
+    @property
+    def __json__(self):
+        output = {
+            "printbed_heatable": self.printbed_heatable
+        }
+        return output
 
 
 class Machine(models.Model):
@@ -66,6 +95,21 @@ class Machine(models.Model):
 
     def __str__(self):
         return self.model
+
+    @property
+    def __json__(self):
+        output = {
+            "model": self.model,
+            "buildarea_maxdim1": self.buildarea_maxdim1,
+            "buildarea_maxdim2": self.buildarea_maxdim2,
+            "form": self.form,
+            "temperature_controllers": {
+                "extruder": self.extruder.__json__
+            },
+            "chamber": self.chamber.__json__,
+            "printbed": self.printbed.__json__
+        }
+        return output
 
 
 class Settings(models.Model):
@@ -103,6 +147,34 @@ class Settings(models.Model):
     def __str__(self):
         return self.name
 
+    @property
+    def __json__(self):
+        output = {
+            "speed_travel": self.speed_travel,
+            "raft_density": self.raft_density,
+            "speed_printing_raft": self.speed_printing_raft,
+            "track_height": self.track_height,
+            "track_height_raft": self.track_height_raft,
+            "track_width": self.track_width,
+            "track_width_raft": self.track_width_raft,
+            "extrusion_multiplier": self.extrusion_multiplier,
+            "temperature_extruder": self.temperature_extruder,
+            "temperature_extruder_raft": self.temperature_extruder_raft,
+            "retraction_restart_distance": self.retraction_restart_distance,
+            "retraction_speed": self.retraction_speed,
+            "retraction_distance": self.retraction_distance,
+            "bridging_extrusion_multiplier": self.bridging_extrusion_multiplier,
+            "bridging_part_cooling": self.bridging_part_cooling,
+            "bridging_speed_printing": self.bridging_speed_printing,
+            "speed_printing": self.speed_printing,
+            "coasting_distance": self.coasting_distance,
+            "critical_overhang_angle": self.critical_overhang_angle,
+            "temperature_printbed_setpoint": self.temperature_printbed_setpoint,
+            "temperature_chamber_setpoint": self.temperature_printbed_setpoint,
+            "part_cooling_setpoint": self.part_cooling_setpoint
+        }
+        return output
+
 
 class Session(models.Model):
     number = models.IntegerField(default=0)
@@ -115,37 +187,66 @@ class Session(models.Model):
     material = models.ForeignKey(Material, on_delete=models.CASCADE, null=False)
     settings = models.ForeignKey(Settings, on_delete=models.CASCADE, null=False)
 
+    def save(self, **kwargs):
+        super(Session, self).save(**kwargs)
+
     def __str__(self):
         return "{} (target: {})".format(self.name, self.target)
 
+    @property
+    def __json__(self):
+        output = {
+            "machine": self.machine.__json__,
+            "material": self.material.__json__,
+            "session": {
+                "uid": 211,
+                "target": self.target,
+                "test_number": "03",
+                "min_max_parameter_one": [],
+                "min_max_parameter_two": [
+                    40,
+                    100
+                ],
+                "min_max_parameter_three": [],
+                "test_type": "A",
+                "user_id": "user name",
+                "offset": [
+                    0,
+                    0
+                ],
+                "slicer": self.slicer,
+                "previous_tests": []
+            },
+            "settings": self.settings.__json__
+        }
+        return output
 
-# class Test(models.Model):
-#     test_name = models.CharField(max_length=64, choices=[(x, x) for y, x in TEST_NUMBER_CHOICES])
-#     test_number = models.CharField(max_length=64, choices=[(y, y) for y, x in TEST_NUMBER_CHOICES])
-#     executed = models.BooleanField(default=True)
-#     tested_parameter_one_values = None
-#     tested_parameter_two_values = None
-#     tested_parameter_three_values = None
-#     tested_volumetric_flow_rate_values = None
-#     selected_parameter_one_value = models.DecimalField(max_digits=4, decimal_places=3, default=0, blank=True)
-#     selected_parameter_two_value = models.DecimalField(max_digits=4, decimal_places=3, default=0, blank=True)
-#     selected_parameter_three_value = models.DecimalField(max_digits=4, decimal_places=3, default=0, blank=True)
-#     selected_volumetric_flow_rate_value = models.DecimalField(max_digits=4, decimal_places=3, default=0, blank=True)
-#     # "selected_parameter_one_value": 0.375,
-#     # "selected_parameter_two_value": 25,
-#     # "selected_volumetric_flow-rate_value": 8.432,
-#     # "parameter_one_name": "first-layer track height",
-#     # "parameter_two_name": "first-layer printing speed",
-#     # "parameter_one_units": "mm",
-#     # "parameter_two_units": "mm/s",
-#     # "parameter_one_precision": "{:.3f}",
-#     # "parameter_two_precision": "{:.1f}",
-#     # "gcode_path": "G:\\3d_printing\\Procedure\\OOP\\mp-testing-suite-v2\\gcodes\\212_01.gcode",
-#     # "label_path": "G:\\3d_printing\\Procedure\\OOP\\mp-testing-suite-v2\\pngs\\212_01.png",
-#     # "comments": 0,
-#     # "datetime_info": "2019-07-18 09:15:06",
-#     # "extruded_filament_mm": 841.53,
-#     # "estimated_printing_time": "0:09:28"
+
+class TestInfo(models.Model):
+    test_name = models.CharField(max_length=64, choices=[(x, x) for y, x in TEST_NUMBER_CHOICES])
+    test_number = models.CharField(max_length=64, choices=[(y, y) for y, x in TEST_NUMBER_CHOICES])
+    executed = models.BooleanField(default=True)
+
+    # Save raw json strings to be serialized/unserialized
+    tested_parameter_one_values = models.TextField(max_length=10000, blank=True)
+    tested_parameter_two_values = models.TextField(max_length=10000, blank=True)
+    tested_parameter_three_values = models.TextField(max_length=10000, blank=True)
+    tested_volumetric_flow_rate_values = models.TextField(max_length=10000, blank=True)
+
+    selected_parameter_one_value = models.DecimalField(max_digits=4, decimal_places=3, default=0, blank=True)
+    selected_parameter_two_value = models.DecimalField(max_digits=4, decimal_places=3, default=0, blank=True)
+    selected_parameter_three_value = models.DecimalField(max_digits=4, decimal_places=3, default=0, blank=True)
+    selected_volumetric_flow_rate_value = models.DecimalField(max_digits=4, decimal_places=3, default=0, blank=True)
+    parameter_one_name = models.CharField(max_length=64, default="first-layer track height")
+    parameter_two_name = models.CharField(max_length=64, default="first-layer printing speed")
+    parameter_one_units = models.CharField(max_length=12, choices=UNITS, default="mm")
+    parameter_two_units = models.CharField(max_length=12, choices=UNITS, default="mm/s")
+    parameter_one_precision = "{:.3f}"
+    parameter_two_precision = "{:.1f}"
+    comments = 0
+    datetime_info = models.DateTimeField(default=datetime.now, blank=True)
+    extruded_filament_mm = 841.53
+    estimated_printing_time = "0:09:28"
 
 
 # {
