2cbe44c4f1a4c4299a876a432ed2541db9d6ab27
egilsar <egils.arajs@massportal.com>
2019-08-22 17:04:01 +0300

    Incorporated optimizer API test_info endpoint

--- optimizer_api.py
+++ optimizer_api.py
@@ -1,6 +1,7 @@
 import requests
 from base64 import b64decode
 import json
+from config import config
 
 
 class ApiClient(object):
@@ -10,6 +11,7 @@ class ApiClient(object):
         self.port = port
         self.gcode = None
         self.response = None
+        self.test_info = None
 
     def get_response(self, **kwargs):
         response = requests.post(self.base_url, json=kwargs)
@@ -18,14 +20,20 @@ class ApiClient(object):
     def get_template(self):
         self.get_response()
         if self.response.status_code == 200:
-            self.persistence = json.loads(self.response.text)["persistence"]
+            response = json.loads(self.response.text)
+            self.persistence = response["persistence"]
+            self.test_info = response["test_info"]
             return self.persistence
+
+    def get_test_info(self, persistence: dict):
+        return json.loads(requests.post(self.base_url + "/test_info", json=persistence).text)
 
     def get_gcode(self, arguments):
         self.get_response(**arguments)
         if self.response.status_code == 200:
             response_parsed = json.loads(self.response.text)
             self.persistence = response_parsed["persistence"]
+            self.test_info = response_parsed["test_info"]
             if response_parsed["content"] is not None:
                 # A gcode file has been returned
 
@@ -41,7 +49,10 @@ class ApiClient(object):
         return "http://" + self.url + ":{}".format(str(self.port))
 
 
+api_client = ApiClient(config["OPTIMIZER_DNS"])
+
+
 if __name__ == "__main__":
-    api = ApiClient("ec2-54-93-100-66.eu-central-1.compute.amazonaws.com")
+    api = ApiClient("127.0.0.1", port=5000)
     with open("212.json") as jsonfile:
         session212 = json.load(jsonfile)
