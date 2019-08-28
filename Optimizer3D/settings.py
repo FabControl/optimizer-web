ee0999d02a0995110e0b7ea159e4b8b7d2343c16
egilsar <egils.arajs@massportal.com>
2019-08-28 11:17:56 +0300

    Defug = False for remote server

--- Optimizer3D/settings.py
+++ Optimizer3D/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["dev.3doptimizer.com", "test.3doptimizer.com", "3doptimizer.com", "3.122.252.11", "127.0.0.1"]
 
