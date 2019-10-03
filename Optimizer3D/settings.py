8e10bdfd5477818d4175e96d18119d684337ac72
egilsar <egils.arajs@massportal.com>
2019-10-03 12:43:14 +0300

    Moved fixtures to their respective apps

--- Optimizer3D/settings.py
+++ Optimizer3D/settings.py
@@ -16,8 +16,6 @@ from config import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-FIXTURE_DIRS = (os.path.join(BASE_DIR, 'fixtures'),)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
