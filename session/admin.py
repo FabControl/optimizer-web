f3984fc7a0f9993434986c43c4a176e7d62efc83
egilsar <egils.arajs@massportal.com>
2019-10-04 11:27:12 +0300

    Improved admin interface to display sortable information about testing sessions

--- session/admin.py
+++ session/admin.py
@@ -6,9 +6,17 @@ from .models import *
 
 admin.site.register(Material)
 admin.site.register(Machine)
-admin.site.register(Session)
 admin.site.register(Nozzle)
 admin.site.register(Extruder)
 admin.site.register(Chamber)
 admin.site.register(Printbed)
 admin.site.register(Settings)
+
+
+@admin.register(Session)
+class SessionAdmin(admin.ModelAdmin):
+    list_display = ('name', 'pub_date', 'owner', 'material', 'completed_tests')
+    search_fields = ('name', 'owner__email', 'material__name')
+    sortable_by = ('name', 'pub_date', 'owner', 'material', 'completed_tests')
+    date_hierarchy = 'pub_date'
+
