bd54e4178478c315dd1586dc4e3448388b3d7158
egilsar <egils.arajs@massportal.com>
2019-10-04 17:23:48 +0300

    Implemented custom 404 and 500 error pages

--- Optimizer3D/views.py
+++ Optimizer3D/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render, get_object_or_404, redirect
 
 def index(request):
     return redirect('session:index')
+
+
+def error_404_view(request, exception):
+    return render(request, "404.html")
+
+
+def error_500_view(request, exception):
+    return render(request, "500.html")
