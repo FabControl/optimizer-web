181362e9fd61c2c48831dfad29a89371437303e5
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2019-10-23 12:38:16 +0300

    Added password recovery check
    Testing email sent to address, that was not found in database

--- tests/authentication/test_forms.py
+++ tests/authentication/test_forms.py
@@ -1,5 +1,5 @@
 from django.core import mail
-from authentication.forms import SignUpForm
+from authentication.forms import SignUpForm, ResetPasswordForm
 from django.test import TestCase
 
 
@@ -38,3 +38,28 @@ class SignUpFormTest(TestCase):
         self.assertFalse('SomeVerySecretPassword' in m.body)
         self.assertFalse('SomeVerySecretPassword' in m.alternatives[0][0])
 
+
+class ResetPasswordFormTest(TestCase):
+    req = FakeHttpRequest()
+    req.META = {'HTTP_HOST':'test.local.domain'}
+
+
+    def test_unknown_account_message(self):
+        form = ResetPasswordForm({
+            'email':'someone@somewhere.com'
+            })
+
+        form.full_clean()
+        form.save(self.req)
+
+        # should send single message to single recipent
+        self.assertEqual(len(mail.outbox), 1)
+        m = mail.outbox[-1]
+        self.assertEqual(m.to, ['someone@somewhere.com'])
+        self.assertEqual(m.subject, 'Password recovery')
+        # message should contain plain body and html alternative
+        self.assertEqual(len(m.alternatives), 1)
+        self.assertEqual(m.alternatives[0][1], 'text/html')
+        # If account was not in database, user email should be included
+        self.assertTrue('someone@somewhere.com' in m.body)
+        self.assertTrue('someone@somewhere.com' in m.alternatives[0][0])
