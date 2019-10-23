ab43da9fdc99974ab787b76c4c9788950a1174b6
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2019-10-23 13:32:38 +0300

    Added Email message test in case of valid user account

--- tests/authentication/test_forms.py
+++ tests/authentication/test_forms.py
@@ -1,6 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.core import mail
 from authentication.forms import SignUpForm, ResetPasswordForm
 from django.test import TestCase
+import re
 
 
 # Empty helper class
@@ -42,6 +44,8 @@ class SignUpFormTest(TestCase):
 class ResetPasswordFormTest(TestCase):
     req = FakeHttpRequest()
     req.META = {'HTTP_HOST':'test.local.domain'}
+    reset_link_match = re.compile(re.escape('https://test.local.domain') +
+            '/reset/[0-9a-zA-Z]{2}/[0-9a-z-]{24}/')
 
 
     def test_unknown_account_message(self):
@@ -63,3 +67,29 @@ class ResetPasswordFormTest(TestCase):
         # If account was not in database, user email should be included
         self.assertTrue('someone@somewhere.com' in m.body)
         self.assertTrue('someone@somewhere.com' in m.alternatives[0][0])
+
+    def test_known_account_message(self):
+        user = get_user_model().objects.create_user(email='someone@somewhere.com',
+                                 password='SomeSecretPassword')
+
+        form = ResetPasswordForm({
+            'email':'someone@somewhere.com'
+            })
+
+        form.full_clean()
+        form.save(self.req)
+
+        # should send single message to single recipent
+        self.assertEqual(len(mail.outbox), 1)
+        m = mail.outbox[-1]
+        self.assertEqual(m.to, ['someone@somewhere.com'])
+        self.assertEqual(m.subject, 'Password recovery')
+        # message should contain plain body and html alternative
+        self.assertEqual(len(m.alternatives), 1)
+        self.assertEqual(m.alternatives[0][1], 'text/html')
+        # If account is in database, user should receive recovery link
+        self.assertFalse(self.reset_link_match.search(m.body) is None)
+        self.assertFalse(self.reset_link_match.search(m.alternatives[0][0]) is None)
+        # Password should never be included in message
+        self.assertFalse('SomeVerySecretPassword' in m.body)
+        self.assertFalse('SomeVerySecretPassword' in m.alternatives[0][0])
