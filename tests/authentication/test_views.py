4c188408d79a12b11b5fea0ec473addf73908be6
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2019-10-24 17:36:38 +0300

    Added setup and teardown for Password reset view test

--- tests/authentication/test_views.py
+++ tests/authentication/test_views.py
@@ -1,23 +1,27 @@
 from django.test import TestCase, Client
+from django.contrib.auth import get_user_model
 from django.urls import reverse
 from django.conf import settings
 
 
 class PasswordResetViewsTest(TestCase):
-
-    def set_up(self):
-        self.client = Client()
+    @classmethod
+    def setUpClass(self):
+        self.test_client = Client(HTTP_HOST=settings.ALLOWED_HOSTS[0])
         self.user = get_user_model().objects.create_user(email='known_user@somewhere.com',
                                  password='SomeSecretPassword')
 
+    @classmethod
+    def tearDownClass(self):
+        self.user.delete()
+
+
     def test_account_email_leak(self):
         reset_url = reverse('password_reset')
-        known_response = self.client.post(reset_url, {'email':'known_user@somewhere.com'},
-                                          follow=True,
-                                          HTTP_HOST=settings.ALLOWED_HOSTS[0])
-        unknown_response = self.client.post(reset_url, {'email':'other_user@somewhere.com'},
-                                          follow=True,
-                                          HTTP_HOST=settings.ALLOWED_HOSTS[0])
+        known_response = self.test_client.post(reset_url, {'email':'known_user@somewhere.com'},
+                                          follow=True)
+        unknown_response = self.test_client.post(reset_url, {'email':'other_user@somewhere.com'},
+                                          follow=True)
 
         # Both requests should have equal behavior
         self.assertEqual(known_response.status_code, unknown_response.status_code)
