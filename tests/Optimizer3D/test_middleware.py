bf569580d08ab7e2bae4cdb4a2ba28165a4bcba2
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2019-12-09 15:10:14 +0200

    Added middleware test for previous commit

---
+++ tests/Optimizer3D/test_middleware.py
@@ -0,0 +1,40 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from datetime import timedelta
+
+
+class SubscriptionMiddlewareTest(TestCase):
+    def test_premium_expiration(self):
+        user = get_user_model().objects.create_user(email='known_user@somewhere.com',
+                                 is_active=True,
+                                 plan='premium',
+                                 password='SomeSecretPassword')
+
+
+        self.assertTrue(self.client.login(email='known_user@somewhere.com', password='SomeSecretPassword'))
+        tst_url = reverse('dashboard')
+        self.client.get(tst_url)
+
+        user.refresh_from_db()
+        # By default, premium should already be expired
+        self.assertEqual(user.plan, 'basic')
+
+        user.plan = 'premium'
+        user.subscription_expiration = timezone.now()
+        user.save()
+        self.client.get(tst_url)
+        user.refresh_from_db()
+
+        # should expire, because request was called after expiration was assigned
+        self.assertEqual(user.plan, 'basic')
+
+        user.plan = 'premium'
+        user.subscription_expiration = timezone.now() + timedelta(seconds=2)
+        user.save()
+        self.client.get(tst_url)
+        user.refresh_from_db()
+
+        # should not expire, since we have few extra seconds
+        self.assertEqual(user.plan, 'premium')
