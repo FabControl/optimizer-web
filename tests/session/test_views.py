ad8597704c33910bbbb79b164d9271485543974d
Aivars Vaivods <aivars.vaivods@edu.rtu.lv>
2019-12-10 15:25:54 +0200

    Added dashboard view test
    Making sure, that last few testing seeeins are available from dashboard

--- tests/session/test_views.py
+++ tests/session/test_views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 from django.urls import reverse
+from session import models
 
 
 class SessionViewsTest(TestCase):
@@ -27,4 +28,40 @@ class SessionViewsTest(TestCase):
         # dashboard should be used as index page
         self.assertTrue(len(resp.redirect_chain) > 0)
         self.assertTrue(resp.redirect_chain[-1][0] == reverse('dashboard'))
+
+    def test_dashboard(self):
+        test_sessions = 'Test one;Test two;Test three;Test four;Test five'.split(';')
+        test_url = reverse('dashboard')
+
+        self.assertTrue(self.client.login(email='known_user@somewhere.com', password='SomeSecretPassword'))
+
+        resp = self.client.get(test_url)
+        for s in test_sessions:
+            self.assertFalse(bytes(s, 'utf-8') in resp.content)
+
+        nozzle = models.Nozzle.objects.create()
+        extruder = models.Extruder.objects.create(nozzle=nozzle)
+        chamber = models.Chamber.objects.create()
+        printbed = models.Printbed.objects.create()
+        machine = models.Machine.objects.create(owner=self.user, extruder=extruder,
+                                                chamber=chamber, printbed=printbed)
+
+        material = models.Material(owner=self.user, name='material')
+        material.save()
+
+        sett = models.Settings()
+        sett.save()
+        # create testing sessions
+        for s in test_sessions:
+            models.Session.objects.create(owner=self.user,
+                                          name=s,
+                                          machine=machine,
+                                          material=material,
+                                          _persistence='{"session":{"previous_tests":[]}}',
+                                          settings=sett)
+
+        # check if new sessions are visible in dashboard
+        resp = self.client.get(test_url)
+        for s in test_sessions:
+            self.assertTrue(bytes(s, 'utf-8') in resp.content)
 
