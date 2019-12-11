12fa6ba6c88772a6751a4e278fe134f36a80b028
egilsar <egils.arajs@massportal.com>
2019-12-11 14:54:13 +0200

    Changed log file location to /var/log/httpd/django.log

--- Optimizer3D/settings.py
+++ Optimizer3D/settings.py
@@ -131,7 +131,7 @@ LOGGING = {
         },
         'logfile': {
             'class': 'logging.FileHandler',
-            'filename': 'django.log'
+            'filename': '/var/log/httpd/django.log'
         },
     },
     'loggers': {
